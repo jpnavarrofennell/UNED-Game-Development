200ca6df67789875def4a53939222f7018daa4cf
Juan Pablo Navarro Fennell <juan.navarro@tayassugames.com>
2018-07-12 18:15:18 -0600

    Avances varios en el personaje

--- Assets/Scripts/Sokoban/SKPlayer.cs
+++ Assets/Scripts/Sokoban/SKPlayer.cs
@@ -9,9 +9,11 @@ namespace UnedSokoban
         public float threshold;
         private SKSmoothFollow _smoothFollow;
         private Vector3 _direction;
+        private bool _isReadyToMove;
 
         void Start()
         {
+            _isReadyToMove = true;
             this.gameObject.transform.parent.name = "Ground";
             this.gameObject.name = "Player";
             this.gameObject.transform.parent = null;
@@ -19,24 +21,48 @@ namespace UnedSokoban
             _smoothFollow.target = this.gameObject.transform;
         }
 
-        // Update is called once per frame
         void Update()
-        {
-            _direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        { 
+            if(Input.GetKeyDown(KeyCode.RightArrow)) 
+            {
+                MoveChar(Vector3.right);
+            }
 
-            MoveChar(_direction);
+            if(Input.GetKeyDown(KeyCode.LeftArrow)) 
+            {
+                MoveChar(Vector3.left);
+            }
+
+            if(Input.GetKeyDown(KeyCode.DownArrow)) 
+            {
+                MoveChar(Vector3.back);
+            }
+
+            if(Input.GetKeyDown(KeyCode.UpArrow)) 
+            {
+                MoveChar(Vector3.forward);
+            }
         }
 
         public void MoveChar(Vector3 direction)
         {
-            if(DetectObstruction()) 
+            if(!DetectObstruction() && _isReadyToMove) 
             {
-                
+                StartCoroutine(TimedMove(direction));
             }
+        }
+
+        private IEnumerator TimedMove(Vector3 direction) 
+        {
+            _isReadyToMove = false;
+            this.transform.Translate(direction);
+            yield return new WaitForSeconds(0.1f);
+            _isReadyToMove = true;
         }
 
         private bool DetectObstruction() 
         {
+            
             return false;
         }
     }
