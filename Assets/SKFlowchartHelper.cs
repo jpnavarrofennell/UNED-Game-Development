9221b8b980caf4ad2a9ee8c1dcc028efa413f8bb
Juan Pablo Navarro Fennell <juan.navarro@tayassugames.com>
2018-11-09 06:32:21 -0600

    modificaciones en el proyecto

--- Assets/SKFlowchartHelper.cs
+++ Assets/SKFlowchartHelper.cs
@@ -2,24 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Fungus;
-using UnedSokoban;
 
-public class SKFlowchartHelper : MonoBehaviour {
+namespace UnedSokoban
+{
+    public class SKFlowchartHelper : MonoBehaviour
+    {
+        public Flowchart flchart;
 
-    public Flowchart flchart;
-
-	// Use this for initialization
-	void Start () {
-        flchart.SetIntegerVariable("movimientos", SKGameControl.instance.characterMoves);
-        flchart.SetIntegerVariable("bloqueactual", SKGameControl.instance.currentBlock);
-
-        SKGameControl.instance.characterMoves = 0;
-
-        flchart.ExecuteBlock("Main");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        // Use this for initialization
+        void Start()
+        {
+            flchart.SetIntegerVariable("movimientos", SKGameControl.instance.characterMoves);
+            flchart.SetIntegerVariable("bloqueactual", SKGameControl.instance.currentBlock);
+            SKGameControl.instance.characterMoves = 0;
+            flchart.ExecuteBlock("Main");
+        }
+    }
 }
+
